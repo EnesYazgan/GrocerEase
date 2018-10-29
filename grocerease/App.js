9eb143d0a99b4c9d5cac8d63d7f7a084b267fecb
simonbcodes <simonbukin@gmail.com>
2018-10-29 14:19:08 -0700

    Add BarCode scanning functionality

--- grocerease/App.js
+++ grocerease/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner, Permissions } from 'expo';
 
-export default class App extends React.Component {
+export default class BarcodeScannerExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+  }
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === 'granted'});
+  }
+
   render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
     return (
-      <View style={styles.container}>
-        <Text>GrocerEase</Text>
+      <View style={{ flex: 1 }}>
+        <BarCodeScanner
+          onBarCodeScanned={this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFill}
+        />
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  handleBarCodeScanned = ({ type, data }) => {
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  }
+}
