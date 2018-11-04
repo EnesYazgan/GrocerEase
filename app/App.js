d99577a7c129d617f9705547670ec371c975f77c
Enes Yazgan <enes32y@gmail.com>
2018-11-04 11:15:01 -0800

    updating app.js

--- app/App.js
+++ app/App.js
@@ -1,35 +1,86 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, StyleSheet } from 'react-native';
-import List from './components/List.js';
+import { AppRegistry, Text, TextInput, View, Button, StyleSheet, FlatList, StatusBar} from 'react-native';
+import SearchList from '.components/SearchList';
+import BarcodeScanner from './components/BarcodeScanner'
 
-export default class TextInANest extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      titleText: "Bird's Nest",
-      bodyText: 'This is not really a bird nest.'
+      inventory: [], //for the ingredients list
     };
   }
 
   render() {
     return (
-	  //List.insert(titleText);
-      <Text style={styles.baseText}>
-		//List.insert(titleText)
-        <Text style={styles.titleText} onPress={this.onPressTitle}>
-          {List.head}{'\n'}{'\n'}
-        </Text>
-        <Text numberOfLines={5}>
-          {this.state.bodyText}
-        </Text>
-      </Text>
+      <View style={styles.container}>
+        
+         <BarcodeScanner />
+        <SearchList
+        inventory = {this.state.inventory}
+        />
+      </View>
     );
   }
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "0%"
+        }}
+      />
+    );
+  };
 }
 
 const styles = StyleSheet.create({
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  banner: {
+    backgroundColor: 'green',
   },
+  headerText: {
+    flexDirection: 'row',
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  searchBar: {
+    flex: 1,
+    fontSize: 20,
+    padding: 10,
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  icon: {
+    marginRight: 10,
+  }
 });
