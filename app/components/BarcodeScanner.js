739b28491e28cd86d633afb360c25f47eef11f78
simonbcodes <simonbukin@gmail.com>
2018-11-06 12:47:25 -0800

    Fix barcode scanning multiple times + send HTTP requests

--- app/components/BarcodeScanner.js
+++ app/components/BarcodeScanner.js
@@ -5,6 +5,8 @@ import { BarCodeScanner, Permissions } from 'expo';
 export default class BarcodeScanner extends React.Component {
   state = {
     hasCameraPermission: null,
+    scannedCode: null,
+    codeType: null
   }
 
   async componentWillMount() {
@@ -31,7 +33,31 @@ export default class BarcodeScanner extends React.Component {
     );
   }
 
+  lookup() {
+    url = 'https://www.upcindex.com/' + this.state.scannedCode;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === 4) {
+          if (request.status === 200) {
+              document.body.className = 'ok';
+              console.log(request.responseText);
+          } else if (!isValid(this.response) && this.status == 0) {
+              document.body.className = 'error offline';
+              console.log("The computer appears to be offline.");
+          } else {
+              document.body.className = 'error';
+          }
+      }
+    };
+    request.open("GET", url, true);
+    request.send(null);
+  }
+
   handleBarCodeScanned = ({ type, data }) => {
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    if (type != this.state.codeType || data != this.state.scannedCode) { //checks if barcode is the same as the one already scanned
+      // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+      this.setState({scannedCode: data, codeType: type})
+      alert(this.lookup(this.state.scannedCode))
+    }
   }
 }
