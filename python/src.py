ef06475c7ee7d232b1b322090d4c5d729c14144f
simonbcodes <simonbukin@gmail.com>
2018-11-19 08:51:34 -0800

    Add basic workflow for webscraping + writing JSON output

--- python/src.py
+++ python/src.py
@@ -1,5 +1,33 @@
 import lxml.html
 import requests
+import json
+
+
+class Recipe:
+    def __init__(self,
+                 url,
+                 title,
+                 image_src,
+                 ingredients,
+                 equipment_dict,
+                 steps):
+                    self.url = url
+                    self.title = title
+                    self.image_src = image_src
+                    self.ingredients = ingredients
+                    self.equipment_dict = equipment_dict
+                    self.steps = steps
+    # converts object to JSON string
+    def toJSON(self):
+        return json.dumps(self,
+                          default=lambda o: o.__dict__,
+                          sort_keys=True,
+                          indent=4)
+
+
+def stringUTF8(string):
+    return str(string.text_content().encode('ascii', 'ignore').decode("utf-8"))
+
 
 # URL that we are parsing
 url = 'https://www.bonappetit.com/recipe/basically-spaghetti-pomodoro'
@@ -19,6 +47,9 @@ image_src = image_src[0].split(' ')[0]
 # get ingredients
 ingredients = html.xpath('//li[@class="ingredient"]/label/div')
 
+# grab text from HTML elements
+ingredients = [stringUTF8(ingredient) for ingredient in ingredients]
+
 # get equipment necessary
 equip_links = html.xpath('//div[@class="image-grid-item-image"]/div/a/@href')
 equip_names = html.xpath('//div[@class="image-grid-item-text"]/text()')
@@ -30,4 +61,11 @@ equipment = dict(zip(equip_names, equip_links))
 steps = html.xpath('//li[@class="step"]/div/p')
 
 # use list comprehension to run text_content on all steps
-steps = [step.text_content() for step in steps]
+steps = [stringUTF8(step) for step in steps]
+
+# create new Recipe object with scraped data
+spaghetti = Recipe(url, title, image_src, ingredients, equipment, steps)
+
+# write to file
+with open('spaghetti.json', 'w') as file:
+    file.write(spaghetti.toJSON())
